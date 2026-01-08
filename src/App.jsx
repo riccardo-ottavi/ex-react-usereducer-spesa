42b636d048fac206337b7b6cbc9c2f8dcdfb8ffd
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-08 15:13:10 +0100

    inizializza componente lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import ProductsList from './components/ProductsList';
 
 function App() {
+
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
 
   return (
     <>
       <h1>Prova</h1>
+      <ProductsList/>
     </>
   )
 }
---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,7 @@
+export default function ProductsList(){
+    return(
+        <div className="container">
+            <h2>Lista Prodotti</h2>
+        </div>
+    )
+}
